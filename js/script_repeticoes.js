d997cbc68ac451b7cbc28bb31ecaa6116cdf37e7
Marcos <marcos.k.santos6@aluno.senai.br>
2026-06-16 16:43:42 -0300

    CONTADOR E ACUMULADOR CRIADOS

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -3,4 +3,13 @@ const divFor = document.querySelector('#div-for')
 
 for(i = 0;i < 20; i++ ){
     console.log(i, 'lorena')
+
+    divFor.innerHTML += `${i} - Lorena <br>`
 }
+
+//CONTADOR/ACUMULADOR
+
+const ImputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContNum = document.querySelector('#div-cont-acum')
+
